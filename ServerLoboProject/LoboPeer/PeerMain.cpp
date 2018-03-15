4d4456c7d444ee6e994f22c1103de04a46d731f6
Alex <a.canutperdigon@gmail.com>
2018-03-15 14:50:02 +0100

    El server bootstrap está preparado. El peer está en proceso.

--- ServerLoboProject/LoboPeer/PeerMain.cpp
+++ ServerLoboProject/LoboPeer/PeerMain.cpp
@@ -1,3 +1,32 @@
+#include <iostream>
+#include <string>
+#include <SFML\Network.hpp>
+#include <Player.h>
+
 int main() {
-	return 0;
+	Player::ROLE myRole;
+	int myID;
+	std::string username;
+	std::cout << "Introduce el nombre de usuario" << std::endl;
+	std::cin >> username;
+
+	sf::TcpSocket* socket = new sf::TcpSocket();
+	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+	sf::Socket::Status status;
+
+	status = socket->connect(ip, 50000);
+	if (status != sf::Socket::Done) {
+		std::cout << "Error al conectar con el servidor" << std::endl;
+	}
+	else {
+		sf::Packet incomingInfo;
+		status = socket->receive(incomingInfo);
+		if(status == sf::Socket::Done){
+			std::string command;
+			incomingInfo >> command;
+			if (command == "INFOS_") {
+				incomingInfo >> myRole;
+			}
+		}
+	}
 }
--- ServerLoboProject/ServerBootstrap/ServerBootstrapMain.cpp
+++ ServerLoboProject/ServerBootstrap/ServerBootstrapMain.cpp
@@ -1,3 +1,84 @@
+#include <SFML\Network.hpp>
+#include <iostream>
+#include <Player.h>
+
+class Direction {
+	std::string ip, name;
+	int port, id;
+public:
+	Direction(std::string ip, int port, std::string username, int id) {
+		this->ip = ip;
+		this->port = port;
+		name = username;
+		this->id = id;
+	}
+
+	std::string GetIP() {
+		return ip;
+	}
+	std::string GetName() {
+		return name;
+	}
+	int GetPort() {
+		return port;
+	}
+	int GetID() {
+		return id;
+	}
+};
+
 int main() {
-	return 0;
+	sf::TcpListener listener;
+	sf::Socket::Status status;
+	int maxPlayers = 12;
+	std::vector<Direction> aDirections;
+
+	status = listener.listen(50000);
+	if (status = sf::TcpListener::Done) {//SE HA PODIDO VINCULAR BIEN AL PUERTO
+		for (int i = 0; i < maxPlayers; i++) {
+			sf::TcpSocket* socket = new sf::TcpSocket();
+			status = listener.accept(*socket);
+			
+			if (status == sf::Socket::Done){//SE HA CONECTADO A UN PEER
+				sf::Packet incomingInfo;
+				status = socket->receive(incomingInfo);
+				if (status == sf::Socket::Done) { //RECEPCION DEL MENSAJE DE PRESENTACION DEL PEER
+					std::string comando;
+					incomingInfo >> comando;
+					if (comando == "NAME_") { //COMANDO DE COMUNICACION "NAME_"
+						//el peer manda su nombre de usuario y lo guardamos en la informacion de este peer
+						std::string username;
+						incomingInfo >> username;
+						sf::Packet infoPeers;
+
+						int size = aDirections.size();
+						int playerID = i;
+
+						infoPeers << "INFOS_" << Player::ROLE::_VILLAGER << playerID << size;
+						for (int i = 0; i < size; i++) {
+							infoPeers << aDirections[i].GetIP() << aDirections[i].GetPort() << playerID << username;
+						}
+						status = socket->send(infoPeers); //se envia la informacion de todos los peers que se habian conectado antes
+						if (status != sf::Socket::Done) {
+							std::cout << "Error al enviar el mensaje" << std::endl;
+						}
+						else {
+							//se añade la informacion de este peer
+							aDirections.push_back(Direction(socket->getRemoteAddress().toString(), socket->getRemotePort(),username,playerID));
+						}
+						socket->disconnect();
+						delete socket;
+					}
+				}
+			} 
+			else {
+				std::cout << "Error al conectar con el peer" << std::endl;
+				delete socket;
+			}
+		}
+	}
+	else {
+		std::cout << "No se ha podido vincular al puerto" << std::endl;
+	}
 }
+
